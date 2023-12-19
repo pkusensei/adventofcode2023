67cb20d859c52f4455e9f848742a1d7cbaea8daa
pkusensei <ZW_08@outlook.com>
2023-12-18 20:50:41 -0500

    retrofitting flood-fill

--- d18/src/lib.rs
+++ d18/src/lib.rs
@@ -40,6 +40,7 @@ fn solve(vertices: &[(isize, isize)], boundary: isize) -> isize {
         .zip(vertices.iter().cycle().skip(1))
         .map(|(a, b)| a.0 * b.1 - a.1 * b.0)
         .sum();
+    // Plug in Pick's theorem
     area.abs() / 2 + boundary / 2 + 1
 }
 
@@ -58,6 +59,59 @@ fn parse<'a>(input: &'a str, re: &'a Regex) -> impl Iterator<Item = [&'a str; 4]
         let (_, group) = cap.extract::<4>();
         group
     })
+}
+
+fn flood_fill(input: &str) -> isize {
+    use std::collections::{HashSet, VecDeque};
+
+    fn proceed_line(curr: (isize, isize), dir: u8, steps: isize) -> Vec<(isize, isize)> {
+        match dir {
+            b'U' | b'3' => (1..=steps).map(|step| (curr.0, curr.1 - step)).collect(),
+            b'D' | b'1' => (1..=steps).map(|step| (curr.0, curr.1 + step)).collect(),
+            b'R' | b'0' => (1..=steps).map(|step| (curr.0 + step, curr.1)).collect(),
+            b'L' | b'2' => (1..=steps).map(|step| (curr.0 - step, curr.1)).collect(),
+            _ => unreachable!(),
+        }
+    }
+    const DIRS: [(isize, isize); 4] = [(1, 0), (-1, 0), (0, 1), (0, -1)];
+
+    let re = Regex::new(RE).unwrap();
+    let mut curr = (0, 0);
+    let mut boundary = HashSet::from([curr]);
+    for [dir, steps, ..] in parse(input, &re) {
+        let line = proceed_line(curr, dir.as_bytes()[0], steps.parse().unwrap());
+        curr = *line.last().unwrap();
+        boundary.extend(line);
+    }
+    // Expand the grid out by one
+    let xmin = boundary.iter().map(|c| c.0).min().unwrap() - 1;
+    let xmax = boundary.iter().map(|c| c.0).max().unwrap() + 1;
+    let ymin = boundary.iter().map(|c| c.1).min().unwrap() - 1;
+    let ymax = boundary.iter().map(|c| c.1).max().unwrap() + 1;
+
+    // Start from topleft point, which is also outside
+    let mut outside = HashSet::from([(xmin, ymin)]);
+    let mut queue = VecDeque::from([(xmin, ymin)]);
+    while let Some((cur_x, cur_y)) = queue.pop_front() {
+        for (dx, dy) in DIRS {
+            // Pick one out of four directions
+            let (x, y) = (cur_x + dx, cur_y + dy);
+            if x < xmin || xmax < x || y < ymin || ymax < y {
+                // Falls out of expanded graph
+                continue;
+            }
+            if outside.contains(&(x, y)) || boundary.contains(&(x, y)) {
+                // Already seen
+                // or
+                // Touches boundary; potentially sticks inside
+                continue;
+            }
+            // Still outside
+            queue.push_back((x, y));
+            outside.insert((x, y));
+        }
+    }
+    (xmax - xmin + 1) * (ymax - ymin + 1) - outside.len() as isize
 }
 
 #[cfg(test)]
@@ -91,5 +145,12 @@ mod tests {
     fn test2() {
         assert_eq!(p2(TEST), 952408144115);
         assert_eq!(p2(INPUT), 44644464596918);
+    }
+
+    #[test]
+    #[ignore = "Retrofitting flood-fill into Part 1"]
+    fn test_flood_fill() {
+        assert_eq!(flood_fill(TEST), 62);
+        assert_eq!(flood_fill(INPUT), 39039);
     }
 }
